46a449a30b56a80743bd3074ceda5d916d8210bd
gareth-a-walters <gareth.a.walters@gmail.com>
2021-07-14 11:55:24 +0100

    SI21-49: Fixed docker compose issues

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Route path='/' component={App} />
+  </Router>,
   document.getElementById('root')
 );
 
