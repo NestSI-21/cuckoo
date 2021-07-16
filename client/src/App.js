641800a755279b43a37110ebece623c8ce2d0271
gareth-a-walters <gareth.a.walters@gmail.com>
2021-07-16 13:54:23 +0100

    SI21-51: Added cuckoos and landing routes. Removed news

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './screens/Landing';
+import SignIn from './screens/SignIn';
 import Dashboard from './screens/Dashboard';
-import SignIn from './screens/Signin';
-import News from './screens/News';
+import Cuckoos from './screens/Cuckoos';
 import Companies from './screens/Companies';
 import Profile from './screens/Profile';
 
@@ -12,9 +13,10 @@ function App() {
         <h1>Cuckoo</h1>
       </div>
       <Switch>
-        <Route path='/' component={Dashboard} exact />
+        <Route path='/' component={Landing} exact />
         <Route path='/signin' component={SignIn} exact />
-        <Route path='/news' component={News} />
+        <Route path='/dashboard' component={Dashboard} exact />
+        <Route path='/cuckoos' component={Cuckoos} />
         <Route path='/companies' component={Companies} />
         <Route path='/profile' component={Profile} />
       </Switch>
---
+++ client/src/screens/Cuckoos/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cuckoos = () => {
+  return (
+    <div>
+      <h1>Cuckoos</h1>
+    </div>
+  );
+};
+
+export default Cuckoos;
