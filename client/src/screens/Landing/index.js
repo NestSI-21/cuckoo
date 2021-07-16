431e93f5b4c0b5546f42281b869be585035aee9c
gareth-a-walters <gareth.a.walters@gmail.com>
2021-07-16 16:33:18 +0100

    SI21-68v2: Fixed issues. Formatted with prettier

--- client/src/screens/Landing/index.js
+++ client/src/screens/Landing/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 const Landing = () => {
   return (
     <div>
       <h1>Landing</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
