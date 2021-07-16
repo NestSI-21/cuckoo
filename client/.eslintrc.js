41fa41436d43ea3e67ddf6dedbe2634bbeb0697f
gareth-a-walters <gareth.a.walters@gmail.com>
2021-07-16 12:18:52 +0100

    SI21-68: Configured eslint and prettier

---
+++ client/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true, // Make sure eslint picks up the config at the root of the directory
+  parserOptions: {
+    ecmaVersion: 12, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true, // Enable JSX since we're using React
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
+    },
+  },
+  env: {
+    browser: true, // Enables browser globals like window and document
+    es2021: true,
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    node: true, // Enables Node.js global variables and Node.js scoping.
+  },
+  plugins: ['react'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+  ],
+  rules: {
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Use our .prettierrc file as source
+    'react/react-in-jsx-scope': 'off',
+  },
+};
--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,26 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function sayHi(name) {
+    const age = 10;
+  }
+
+  const things = ['loll', 'ahaha', 'yeah boy'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
