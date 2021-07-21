9bb2c8ffe92d1697ffdad20abab75bed59af61a8
BarbaraMota4 <barbaramota@outlook.pt>
2021-07-21 11:41:29 +0100

    SI21-3: Started landing page wip

--- client/src/screens/Landing/index.js
+++ client/src/screens/Landing/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { landing } from './landing.module.scss';
+import horizontalLogo from '../../assets/logos/logo.svg';
+import slackIcon from '../../assets/icons/slack.svg';
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing</h1>
+    <div className={landing}>
+      <img src={horizontalLogo} alt='Cuckoo horizontal logo' />
+      <h1>Welcome to Cuckoo</h1>
+      <button className='button'>
+        <img src={slackIcon} alt='Slack·Icon' />
+        Sign in with Slack
+      </button>
     </div>
   );
 };
