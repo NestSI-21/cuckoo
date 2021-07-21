54cfce06da31d784399a80276f218f98c48f8eb3
BarbaraMota4 <barbaramota@outlook.pt>
2021-07-21 12:33:45 +0100

    SI21-3: Finished landing page

--- client/src/screens/Landing/index.js
+++ client/src/screens/Landing/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import { landing } from './landing.module.scss';
+import { landingContent } from './landing.module.scss';
+import { logo } from './landing.module.scss';
 import horizontalLogo from '../../assets/logos/logo.svg';
 import slackIcon from '../../assets/icons/slack.svg';
 
@@ -8,12 +10,14 @@ const Landing = () => {
   return (
     <Layout hideHeaderSidebar>
       <div className={landing}>
-        <img src={horizontalLogo} alt='Cuckoo horizontal logo' />
-        <h1>Welcome to Cuckoo</h1>
-        <button className='button'>
-          <img src={slackIcon} alt='Slack·Icon' />
-          Sign in with Slack
-        </button>
+        <div className={landingContent}>
+          <img src={horizontalLogo} alt='Cuckoo horizontal logo' className={logo} />
+          <h1>Welcome to Cuckoo</h1>
+          <button className='button'>
+            <img src={slackIcon} alt='Slack·Icon' />
+            <span>Sign in with Slack</span>
+          </button>
+        </div>
       </div>
     </Layout>
   );
