763cb59524bd7267a7a54d8aec5e5a4c8f67b55b
Minio10 <herminiom10@gmail.com>
2021-07-28 18:08:30 +0100

    SI21-react-bug react bug

--- client/src/components/EditProfile/index.js
+++ client/src/components/EditProfile/index.js
@@ -1,14 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../../elements/Input';
 import Button from '../../elements/Button';
 import { form } from './editprofile.module.scss';
+// import axios from 'axios';
 
 const EditProfile = () => {
+  const [data, setData] = useState({
+    company: '',
+    role: '',
+    birthdate: {},
+  });
+
+  // const [company, setCompany] = useState("")
+  // const [role, setRole] = useState("")
+  // const [birthdate, setBirthdate] = useState("")
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+    console.log(data);
+  };
+
+  console.log(data);
+
+  // const handleSubmit=(event)=>{
+  //   event.preventDefault()
+  //   // console.log(company,role,birthdate)  // const [company, setCompany] = useState("")
+  // // const [role, setRole] = useState("")
+  // // const [birthdate, setBirthdate] = useState("")
+  //   axios
+  //     .post(`${process.env.REACT_APP_API_BASE_URL}/users/complete_profile`, { company: company, role: role, birthdate: birthdate })
+  //     .then((resp) => console.log(resp));
+  // }
+
   return (
     <form className={form}>
-      <Input type='text' name='company' label='Which company do you work for?' mandatory />
-      <Input type='text' name='role' label='What is your occupation?' mandatory />
-      <Input type='date' name='birthdate' label='Please enter your date of birth' />
+      <Input
+        type='text'
+        name='company'
+        value={data.company}
+        onChange={(e) => handleChange(e)}
+        label='Which company do you work for?'
+        mandatory
+      />
+      <Input
+        type='text'
+        name='role'
+        value={data.role}
+        onChange={handleChange}
+        label='What is your occupation?'
+        mandatory
+      />
+      <Input
+        type='date'
+        name='birthdate'
+        value={data.birthdate}
+        onChange={handleChange}
+        label='Please enter your date of birth'
+      />
       <Button text='Get Started' type='submit' />
     </form>
   );
