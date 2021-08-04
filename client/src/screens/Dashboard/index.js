fbc50920af60ababa7a4d938f8843d6963529899
dbgfribeiro <dribeiro641@gmail.com>
2021-08-04 10:22:54 +0100

    SI21-105 : Implemented cuckoos carousel

--- client/src/screens/Dashboard/index.js
+++ client/src/screens/Dashboard/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Layout from '../../components/Layout';
 import CuckooList from '../../components/CuckooList';
+import CuckooCarousel from '../../components/CuckooCarousel';
 import Calendar from '../../components/Calendar';
 import { contentContainer } from './dashboard.module.scss';
 
 const Dashboard = () => {
+  const width = window.innerWidth;
+  const breakpoint = 501;
+
   return (
     <Layout pageTitle='Dashboard'>
       <div className={contentContainer}>
-        <CuckooList />
+        {width < breakpoint ? <CuckooCarousel /> : <CuckooList />}
         <Calendar />
       </div>
     </Layout>
