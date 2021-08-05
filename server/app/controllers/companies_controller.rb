5b3bfd270746cb156ed62efb6e836dc2692575e7
Minio10 <herminiom10@gmail.com>
2021-08-05 14:27:42 +0100

    SI21-128: Created the endpoint to send the companies information to the frontend

---
+++ server/app/controllers/companies_controller.rb
@@ -0,0 +1,7 @@
+class PostsController < ActionController::API
+    before_action :authenticate_user!
+  
+    def index
+      @companies = Company.all
+      render json: { companies: @companies }, status: :ok
+    end
