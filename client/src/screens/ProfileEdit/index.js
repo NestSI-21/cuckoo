d5658c8c677c3756426b7e280868d25f4abe3fd4
gareth-a-walters <gareth.a.walters@gmail.com>
2021-08-09 11:37:23 +0100

    SI21-126: Removed commented code

--- client/src/screens/ProfileEdit/index.js
+++ client/src/screens/ProfileEdit/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import React, { useState, useEffect } from 'react';
 import Layout from '../../components/Layout';
 import ProfileForm from '../../components/ProfileForm';
 import { contentContainer } from './profileedit.module.scss';
