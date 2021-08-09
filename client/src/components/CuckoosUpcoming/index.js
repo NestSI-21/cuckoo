31c4c5bdd39f23a5f4002761fa9fd7cf57bbaef6
BarbaraMota4 <barbaramota@outlook.pt>
2021-08-09 15:23:55 +0100

    SI21-110: dashboard details

--- client/src/components/CuckoosUpcoming/index.js
+++ client/src/components/CuckoosUpcoming/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {
   upcoming,
+  upcomingTitle,
   upcomingCard,
   upcomingDay,
+  eventsScroll,
   upcomingDescription,
 } from './cuckoosupcoming.module.scss';
 
 const CuckoosUpcoming = ({ selectedCuckoo }) => {
   return (
     <div className={upcoming}>
-      <h3>Upcoming</h3>
+      <h3 className={upcomingTitle}>Upcoming</h3>
+
       {selectedCuckoo.selectedDay ? (
         <div className={upcomingCard}>
           <div className={upcomingDay}>
@@ -30,45 +33,47 @@ const CuckoosUpcoming = ({ selectedCuckoo }) => {
         </div>
       ) : (
         <>
-          <p>This month:</p>
-          <div className={upcomingCard}>
-            <div className={upcomingDay}>
-              <h3>8</h3>
-              <p>Jul</p>
+          <div className={eventsScroll}>
+            <p>This month:</p>
+            <div className={upcomingCard}>
+              <div className={upcomingDay}>
+                <h3>8</h3>
+                <p>Jul</p>
+              </div>
+              <div className={upcomingDescription}>
+                <h4>Event Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+              </div>
             </div>
-            <div className={upcomingDescription}>
-              <h4>Event Name</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <div className={upcomingCard}>
+              <div className={upcomingDay}>
+                <h3>12</h3>
+                <p>Jul</p>
+              </div>
+              <div className={upcomingDescription}>
+                <h4>Event Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+              </div>
             </div>
-          </div>
-          <div className={upcomingCard}>
-            <div className={upcomingDay}>
-              <h3>12</h3>
-              <p>Jul</p>
+            <div className={upcomingCard}>
+              <div className={upcomingDay}>
+                <h3>28</h3>
+                <p>Jul</p>
+              </div>
+              <div className={upcomingDescription}>
+                <h4>Event Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+              </div>
             </div>
-            <div className={upcomingDescription}>
-              <h4>Event Name</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-            </div>
-          </div>
-          <div className={upcomingCard}>
-            <div className={upcomingDay}>
-              <h3>28</h3>
-              <p>Jul</p>
-            </div>
-            <div className={upcomingDescription}>
-              <h4>Event Name</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-            </div>
-          </div>
-          <div className={upcomingCard}>
-            <div className={upcomingDay}>
-              <h3>31</h3>
-              <p>Jul</p>
-            </div>
-            <div className={upcomingDescription}>
-              <h4>Event Name</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+            <div className={upcomingCard}>
+              <div className={upcomingDay}>
+                <h3>31</h3>
+                <p>Jul</p>
+              </div>
+              <div className={upcomingDescription}>
+                <h4>Event Name</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+              </div>
             </div>
           </div>
         </>
