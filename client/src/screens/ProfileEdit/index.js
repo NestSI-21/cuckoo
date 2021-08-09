7e858d113f1f613f6bdfa92bbb8c2b7a6297d257
gareth-a-walters <gareth.a.walters@gmail.com>
2021-08-09 11:27:29 +0100

    SI21-126: Removed commented code

--- client/src/screens/ProfileEdit/index.js
+++ client/src/screens/ProfileEdit/index.js
@@ -5,20 +5,6 @@ import ProfileForm from '../../components/ProfileForm';
 import { contentContainer } from './profileedit.module.scss';
 
 const ProfileEdit = () => {
-  // const user = JSON.parse(localStorage.getItem('data'));
-
-  // useEffect(() => {
-  //   if (user.profile_completed) {
-  //     console.log('hello');
-  //     setData((prevData) => ({
-  //       ...prevData,
-  //       company: user.company_id,
-  //       role: user.company_role,
-  //       birthday: user.birthday,
-  //     }));
-  //   }
-  // }, []);
-
   return (
     <Layout hideSidebar>
       <div className={contentContainer}>
