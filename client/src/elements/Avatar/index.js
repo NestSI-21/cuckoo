7e1143b2f62b91ed60f38ee625d6cb634df1912f
BarbaraMota4 <barbaramota@outlook.pt>
2021-08-10 11:04:56 +0100

    SI21-110: add requested changes

--- client/src/elements/Avatar/index.js
+++ client/src/elements/Avatar/index.js
@@ -1,6 +1,6 @@
+import React from 'react';
 import PropTypes from 'prop-types';
 import { avatar } from './avatar.module.scss';
-import React from 'react';
 
 const Avatar = ({ userImage }) => {
   return <img src={userImage} className={avatar} alt='user avatar' />;
