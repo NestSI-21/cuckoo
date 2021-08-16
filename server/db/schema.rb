7a61179779737bded07802ed6ca5125b18bfc53c
gareth-a-walters <gareth.a.walters@gmail.com>
2021-08-16 11:04:31 +0100

    SI21-138: Schema merge conflict

--- server/db/schema.rb
+++ server/db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2021_08_13_152609) do
-=======
 ActiveRecord::Schema.define(version: 2021_08_13_175534) do
->>>>>>> main
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
