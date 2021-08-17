4c41c77a930a21f090441008d58f2f13e117a7fe
Dinis Marques <dinis.c.mrqs@gmail.com>
2021-08-17 15:49:49 +0100

    Revert "SI21-123 : Form's validation merged with main"

--- client/src/elements/Input/index.js
+++ client/src/elements/Input/index.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { input } from './input.module.scss';
+import { input, inputError, filled, unfilled, star, errorMessage } from './input.module.scss';
 
-const Input = ({ type, name, onChange, label, value, required }) => {
+const Input = ({ type, name, onChange, label, value, mandatory, error }) => {
   return (
-    <div className={input}>
+    <div className={!error ? input : inputError}>
       <input
         type={type}
         name={name}
         onChange={onChange}
         placeholder=' '
         value={value ?? ''}
-        required={required}
+        className={value != '' ? filled : unfilled}
       />
       <label>{label}</label>
+      {mandatory ? (
+        <span className={star}>
+          <i className='fas fa-asterisk'></i>
+        </span>
+      ) : null}
+      {error ? <p className={errorMessage}>{error}</p> : null}
     </div>
   );
 };
@@ -23,7 +29,8 @@ Input.propTypes = {
   name: PropTypes.string,
   onChange: PropTypes.func,
   label: PropTypes.string,
-  required: PropTypes.bool,
+  mandatory: PropTypes.bool,
+  error: PropTypes.string,
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 };
 
