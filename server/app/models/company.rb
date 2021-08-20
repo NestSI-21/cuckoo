54ddf3f249ca9bcf2eb73cf4b5cc6c9b64d6067c
Minio10 <herminiom10@gmail.com>
2021-08-20 16:16:51 +0100

    SI21-152: Companies logo are now being stored with active storage

--- server/app/models/company.rb
+++ server/app/models/company.rb
@@ -2,4 +2,12 @@
 
 class Company < ApplicationRecord
   has_many :users, dependent: :destroy
+  has_one_attached :logo
+
+  def images_url
+    Rails.application.routes.default_url_options = { host: 'cuckoo-backend.herokuapp.com', protocol: 'https' }
+    images.map do |image|
+      Rails.application.routes.url_helpers.url_for(image)
+    end
+  end
 end
