4b5f0dc17c05bb0c87bf6550699d730287c47656
gareth-a-walters <gareth.a.walters@gmail.com>
2021-08-20 11:19:52 +0100

    SI21-150: Fixed requested comments

--- client/src/screens/Companies/index.js
+++ client/src/screens/Companies/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import CompaniesList from '../../components/CompaniesList';
 import Layout from '../../components/Layout';
+// import SearchBar from '../../elements/SearchBar';
 import { contentContainer } from './companies.module.scss';
 
 const Companies = () => {
   return (
     <Layout pageTitle='Companies' hideCreateBtn>
       <div className={contentContainer}>
-        <CompaniesList />
+        {/* TODO: Add search bar, see Cuckoos.js for functionality
+        <div className={searchbar}>
+          <SearchBar />
+        </div> */}
+        <CompaniesList searchTerm={searchTerm} />
       </div>
     </Layout>
   );
