b9493b9556430a646485b5b4096854e00771133a
Minio10 <herminiom10@gmail.com>
2021-08-23 12:02:25 +0100

    main:reverted the cors.rb file

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins ['https://cuckoo-frontend.herokuapp.com/','http://www.cuckoos.io/']
+    origins '*'
     resource '*',
              headers: :any,
              expose: ['Authorization'],
