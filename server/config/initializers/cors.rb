93d81e8313e800219ae775bbdc938c8a82658c5d
Minio10 <herminiom10@gmail.com>
2021-08-23 11:16:26 +0100

    main: Added cuckoos.io to the cors.rb file

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins '*'
+    origins ['https://cuckoo-frontend.herokuapp.com/','http://www.cuckoos.io/']
     resource '*',
              headers: :any,
              expose: ['Authorization'],
