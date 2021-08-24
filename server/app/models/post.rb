832000e122b6843b73dc372a1e45397a79f54f14
Minio10 <herminiom10@gmail.com>
2021-08-24 10:53:58 +0100

    main:test images v2

--- server/app/models/post.rb
+++ server/app/models/post.rb
@@ -53,7 +53,7 @@ class Post < ApplicationRecord
     client.files_upload(
       channels: '#alerts',
       as_user: true,
-      content: 'https://c8.alamy.com/comp/BHNG1N/rubber-duck-gift-BHNG1N.jpg',
+      file: ('https://c8.alamy.com/comp/BHNG1N/rubber-duck-gift-BHNG1N.jpg','image/jpeg')
     )
   end
 
