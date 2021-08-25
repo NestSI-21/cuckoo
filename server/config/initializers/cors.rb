d8c44927779d016ce067c0c46a52b702a0035537
Minio10 <herminiom10@gmail.com>
2021-08-25 11:15:02 +0100

    main: changed the cors.rb file

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -9,7 +9,7 @@
 
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins 'https://cuckoos.io', 'https://cuckoo-frontend.herokuapp.com'
+    origins 'cuckoos.io', 'https://cuckoo-frontend.herokuapp.com'
     resource '*',
              headers: :any,
              expose: ['Authorization'],
