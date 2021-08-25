71b31c6f7000ce969b26256ba92715799db29309
Minio10 <herminiom10@gmail.com>
2021-08-25 11:54:19 +0100

    main: commented a line

--- server/app/controllers/slack_auth_controller.rb
+++ server/app/controllers/slack_auth_controller.rb
@@ -8,7 +8,7 @@ class SlackAuthController < ActionController::API
       code: params[:code],
       client_id: ENV['SLACK_CLIENT_ID'],
       client_secret: ENV['SLACK_CLIENT_SECRET'],
-      redirect_uri: "#{ENV['FRONTEND_HOST']}/api/v1/auth/slack",
+      # redirect_uri: "#{ENV['FRONTEND_HOST']}/api/v1/auth/slack",
       refresh_token: ENV['SLACK_OAUTH_TOKEN']
     }
 
